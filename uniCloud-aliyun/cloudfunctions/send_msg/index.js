51e9f60c308a412a675dafa53b424649566abbbd
maopengchao <1393867317@qq.com>
2023-07-10 18:53:06 +0800

    云函数

---
+++ uniCloud-aliyun/cloudfunctions/send_msg/index.js
@@ -0,0 +1,20 @@
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	uniCloud.httpclient.request("https://oapi.dingtalk.com/robot/send?access_token=340833ce57c2ed4b7f27af165e073fc180d1d71eda848f216d0d29785e5748be", {
+		method:"POST",
+		data: {
+			msgtype: "text",
+			text: {
+				content: event.text || '天地地大干饭最大！！',
+			},
+		},
+		contentType:"json",
+		dataType:"json",
+	}).then(res => {
+		console.log(res);
+	})
+	//返回数据给客户端
+	return event
+};
---
+++ uniCloud-aliyun/cloudfunctions/send_weather/index.js
@@ -0,0 +1,17 @@
+'use strict';
+exports.main = async (event, context) => {
+	const defaultMsg = '起床上班,今天天气：'
+	uniCloud.httpclient.request("https://restapi.amap.com/v3/weather/weatherInfo?city=330100&key=8655632cef5889cd800e7a66a09012f1", {
+		dataType: 'json'
+	}).then(res => {
+		const data = res.data;
+		uniCloud.callFunction({
+			name: 'send_msg',
+			data: {
+				text: defaultMsg + data.status ? JSON.stringify(data.lives[0]) : '获取天气失败咯！自己查查看吧！'
+			}
+		})
+	})
+	//返回数据给客户端
+	return event
+};
